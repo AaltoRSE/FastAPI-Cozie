b5c0ed9cb18439af03b4d09e22430b5da0bc4ee2
Thomas Pfau <github@thomaspfau.de>
2024-09-25 12:29:56 +0300

    Changing csv output file

--- fastapi_server/app/lambdas/researcher_read/lambda_function.py
+++ fastapi_server/app/lambdas/researcher_read/lambda_function.py
@@ -112,12 +112,10 @@ def lambda_handler(
 
     # Convert to CSV
     file, path = tempfile.mkstemp(dir=download_folder, suffix=".zip")
-
-    with open(path, "w") as f:
-        df.to_csv(
-            f,
-            date_format="%Y-%m-%d %H:%M:%S.%f%z",
-            compression={"method": "zip", "archive_name": f"sample.csv"},
-        )
+    df.to_csv(
+        path,
+        date_format="%Y-%m-%d %H:%M:%S.%f%z",
+        compression={"method": "zip", "archive_name": "sample.csv"},
+    )
 
     return os.path.basename(path)
